669e03c27a97a8a988b5da1c43d77e6f4dbcb65d
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-06-14 15:53:56 +0530

    Database seeder updated for default migration data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Travel;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Travel::factory(20)->create([
+            'is_public'  => true,
+            'created_by' => $user->id,
+        ]);
     }
 }
