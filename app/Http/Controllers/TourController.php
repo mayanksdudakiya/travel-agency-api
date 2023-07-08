44404ef0bbb481e3fa849a2af6ea3a6e1620b368
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-07-08 18:53:53 +0530

    Initial TourController added

---
+++ app/Http/Controllers/TourController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TourController extends Controller
+{
+    public function index()
+    {
+        return [];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\TourController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
