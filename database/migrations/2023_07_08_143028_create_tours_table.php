cd79a768dcfc4442bd40d9a55522b12b17044672
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-07-08 21:07:12 +0530

    Tours migration has added

--- database/migrations/2023_07_08_143028_create_tours_table.php
+++ database/migrations/2023_07_08_143028_create_tours_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('tours', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('travel_id')->constrained('travels');
+            $table->string('name');
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->integer('price');
+            $table->auditFields();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tours');
