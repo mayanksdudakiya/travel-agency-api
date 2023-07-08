c3bcf8fc5962178d6271bae744b3ad99e8e8c7df
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-07-08 21:09:17 +0530

    Tour fillable properties added

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tour extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'travel_id',
+        'name',
+        'starting_date',
+        'ending_date',
+        'price',
+        'created_by',
+        'updated_by',
+    ];
 }
